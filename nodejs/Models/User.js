4a41313bfc8fb3ccd9a2adbb7e3458c15d18538c
root <root@server.h-83937.kz>
2020-05-07 09:54:47 +0600

    Websocket changes

--- nodejs/Models/User.js
+++ nodejs/Models/User.js
@@ -1,13 +1,29 @@
 const { Model } = require('../Modules/db');
+const Technic = require('./Technic');
 
 class User extends Model {
     static get tableName() {
         return 'users';
     }
 
+    static relationMappings = {
+        technics: {
+            relation: Model.ManyToManyRelation,
+            modelClass: Technic,
+            join: {
+                from: 'users.id',
+                through: {
+                    // persons_movies is the join table.
+                    from: 'users_technics.user_id',
+                    to: 'users_technics.technic_id'
+                },
+                to: 'technics.id'
+            }
+        }
+    };
 
-    static async getUserByToken(token) {
-        return await User.query().where('token', token).first();
+    static async getUserByToken(token, relations) {
+        return await User.query().withGraphFetched(relations).where('token', token).first();
     }
 }
 
